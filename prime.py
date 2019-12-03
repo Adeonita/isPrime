ea25f2965931be6461dddcdcb16f992d28fe66cc
adeonita <adeonita.sousa@gmail.com>
2019-12-03 16:08:57 -0300

    Implementaçao com while percorrendo de n ate 1

--- prime.py
+++ prime.py
@@ -1,4 +1,27 @@
-numero = int(input("Digite um numero: "))
 
-if( numero == 2 ):
-    print(f"{numero} é primo")c
+def isPrime():
+    number = int(input("Digite um numero: "))
+
+    if( number == 2 ):
+        print(f"{number} é primo")
+    else:
+        temp = number -1
+        prime = False
+        while number >= 1 and temp > 1:
+            result = number % temp
+            print(f"{number} % {temp} = {result}")
+            if(number % temp == 0):
+                prime = False
+                return
+            else:
+                prime = True
+               # print(f"{number} is Prime")
+                
+
+            temp = temp - 1
+    
+
+            
+
+
+isPrime()
